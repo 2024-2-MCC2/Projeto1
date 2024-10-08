f1913885349892c92483957e824e17f57560ff8f
Duda Lucena Miguel <duda.lucena@outlook.com>
2024-10-07 22:01:22 -0300

    Update DivDeDoacao.js

--- src/Frontend/src/Components/DivDeDoacao.js
+++ src/Frontend/src/Components/DivDeDoacao.js
@@ -4,6 +4,7 @@ import React, { useState } from 'react';
 import Cadastro from "../Components/Cadastro";
 import Login from "../Components/Login";
 
+
 const Frame1 = styled.div`
 display: flex;
 flex-direction: column;
@@ -81,17 +82,17 @@ function DivDeDoacao(){
             </Frame1_1>
             <Frame1_2>
                 <Frame1_2_1>
-                    <img src="https://github.com/2024-2-MCC2/Projeto1/blob/main/imagens/Icone%202.png?raw=true"></img>
+                    <img src="https://github.com/2024-2-MCC2/Projeto1/blob/main/src/Frontend/src/Assets/PainelDeDoacao%20-%20Icone%201.png?raw=true"></img>
                     <h3>Decida o valor destinado</h3>
                     <p>Qualquer valor escolhido será de grande ajuda para nossa causa!</p>
                 </Frame1_2_1>
                 <Frame1_2_2>
-                    <img src="https://github.com/2024-2-MCC2/Projeto1/blob/main/imagens/Icone%201.png?raw=true"></img>
+                    <img src="https://github.com/2024-2-MCC2/Projeto1/blob/main/src/Frontend/src/Assets/PainelDeDoacao%20-%20Icone%202.png?raw=true"></img>
                     <h3>Faça Login ou Cadastre-se</h3>
                     <p>venha fazer parte da nossa filiação e ajude nossas ONGs parceiras</p>
                 </Frame1_2_2>
                 <Frame1_2_3>
-                    <img src="https://github.com/2024-2-MCC2/Projeto1/blob/main/imagens/Icone%203.png?raw=true" ></img>
+                    <img src="https://github.com/2024-2-MCC2/Projeto1/blob/main/src/Frontend/src/Assets/PainelDeDoacao%20-%20Icone%203.png?raw=true" ></img>
                     <h3>Escolha a forma de pagamento</h3>
                     <p>Sua doação será redistribuída entre nossas ONGs parceiras</p>
                 </Frame1_2_3>
