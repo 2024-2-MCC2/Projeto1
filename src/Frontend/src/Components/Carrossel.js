e7356f49e6b23553dbe6c2f53e792c4eeff1e26b
Duda Lucena Miguel <duda.lucena@outlook.com>
2024-11-13 19:09:06 -0300

    commit

--- src/Frontend/src/Components/Carrossel.js
+++ src/Frontend/src/Components/Carrossel.js
@@ -1,45 +1,163 @@
 import styled from "styled-components";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Carousel, CarouselCaption, CarouselItem } from 'react-bootstrap';
+import { Link } from "react-router-dom";
+import video_pinguim from "../Assets/Video_Carrossel/video_pinguim.mp4"
+import video_tartaruga from "../Assets/Video_Carrossel/video_tartaruga.mp4"
+import video_cavalo from "../Assets/Video_Carrossel/video_cavalo.mp4"
+import Logo from "../Assets/Logo.png"
 
 const CarrosselContainer = styled.div`
 width: 100%;
+cursor: pointer;
+.no-link-style {
+  color: #fff; /* Herda a cor do elemento pai */
+  text-decoration: none; /* Remove o sublinhado */
+}
 `
+//----------------------------------------------------------------------------------------------
+const ButtonCarrossel_1 = styled.div`
+  background-color: #F2C438;
+  color: #fff;
+  padding: 10px 70px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center; /* Centraliza o conteúdo do botão */
+  align-items: center; /* Centraliza o conteúdo do botão */
+  position: absolute; /* Permite posicionar o botão sobre o vídeo */
+  top: 560px; /* Posiciona o botão no meio verticalmente */
+  left: 350px; /* Posiciona o botão no meio horizontalmente */
+  transform: translate(-50%, -50%); /* Centraliza o botão */
+  z-index: 20; /* Garante que o botão fique acima do vídeo */
+  &:hover {
+    background-color: rgba(242, 196, 56, 0.8); /* 80% de opacidade */
+  }
+  h2{
+    font-size: 35px;
+    text-align: center;
+  }
+`; 
+// -------------------------------------------------------------------------------------
+const ButtonCarrossel_2 = styled.div`
+  background-color: #F2C438;
+  color: #fff;
+  padding: 10px 70px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center; /* Centraliza o conteúdo do botão */
+  align-items: center; /* Centraliza o conteúdo do botão */
+  position: absolute; /* Permite posicionar o botão sobre o vídeo */
+  top: 560px; /* Posiciona o botão no meio verticalmente */
+  left: 700px; /* Posiciona o botão no meio horizontalmente */
+  transform: translate(-50%, -50%); /* Centraliza o botão */
+  z-index: 20; /* Garante que o botão fique acima do vídeo */
+  &:hover {
+    background-color: rgba(242, 196, 56, 0.8); /* 80% de opacidade */
+  }
+  h2{
+    font-size: 35px;
+    text-align: center;
+  }
+`; 
+//---------------------------------------------------------------------------------------------
+const ButtonCarrossel_3 = styled.div`
+  background-color: #F2C438;
+  color: #fff;
+  padding: 10px 70px;
+  border-radius: 10px;
+  display: flex;
+  justify-content: center; /* Centraliza o conteúdo do botão */
+  align-items: center; /* Centraliza o conteúdo do botão */
+  position: absolute; /* Permite posicionar o botão sobre o vídeo */
+  top: 560px; /* Posiciona o botão no meio verticalmente */
+  left: 1000px; /* Posiciona o botão no meio horizontalmente */
+  transform: translate(-50%, -50%); /* Centraliza o botão */
+  z-index: 20; /* Garante que o botão fique acima do vídeo */
+  &:hover {
+    background-color: rgba(242, 196, 56, 0.8); /* 80% de opacidade */
+  }
+  h2{
+    font-size: 35px;
+    text-align: center;
+  }
+`; 
+//--------------------------------------------------------------------------------
+const LogoCarrossel_1 = styled.img`
+  position: absolute; // Posiciona o logo sobre o vídeo
+  top: 200px; // Ajuste a posição vertical (distância do topo)
+  left: 100px; // Ajuste a posição horizontal (distância da esquerda)
+  width: 550px; // Ajuste o tamanho do logo
+  height: auto; // Mantém a proporção da imagem
+  z-index: 10; // Garante que o logo fique acima do vídeo
+`;
+//----------------------------------------------------------------------------------------
+const LogoCarrossel_2 = styled.img`
+  position: absolute; // Posiciona o logo sobre o vídeo
+  top: 200px; // Ajuste a posição vertical (distância do topo)
+  left: 450px; // Ajuste a posição horizontal (distância da esquerda)
+  width: 550px; // Ajuste o tamanho do logo
+  height: auto; // Mantém a proporção da imagem
+  z-index: 10; // Garante que o logo fique acima do vídeo
+`;
+//--------------------------------------------------------------------------------------------
+const LogoCarrossel_3 = styled.img`
+  position: absolute; // Posiciona o logo sobre o vídeo
+  top: 200px; // Ajuste a posição vertical (distância do topo)
+  left: 750px; // Ajuste a posição horizontal (distância da esquerda)
+  width: 550px; // Ajuste o tamanho do logo
+  height: auto; // Mantém a proporção da imagem
+  z-index: 10; // Garante que o logo fique acima do vídeo
+`;
+
 function Carrossel() {
   return (
     <CarrosselContainer>
       <Carousel controls={true} indicatorLabels={true}>
         <CarouselItem>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/800x400?text=Slide+1"
-            alt=""
-          />
-          <CarouselCaption>
-
+        <video className="d-block w-100" autoPlay loop muted>
+            <source src={video_tartaruga} type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
+          <CarouselCaption> 
           </CarouselCaption>
+          <Link to="/CatalogoDeNoticias" className="no-link-style">
+              <ButtonCarrossel_1 variant="primary">
+                  <h2>CATÁLOGO DE NOTÍCIAS</h2>
+              </ButtonCarrossel_1>
+          </Link>
+          <LogoCarrossel_1 src={Logo} alt="Logo" />
         </CarouselItem>
 
         <CarouselItem>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/800x400?text=Slide+2"
-            alt=""
-          />
+        <video className="d-block w-100" autoPlay loop muted>
+            <source src={video_cavalo} type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
           <CarouselCaption>
-
           </CarouselCaption>
+          <Link to="/Animais" className="no-link-style">
+              <ButtonCarrossel_2 variant="primary">
+                  <h2>ANIMAIS EM EXTINÇÃO</h2>
+              </ButtonCarrossel_2>
+          </Link> 
+          <LogoCarrossel_2 src={Logo} alt="Logo" />
         </CarouselItem>
 
         <CarouselItem>
-          <img
-            className="d-block w-100"
-            src="https://via.placeholder.com/800x400?text=Slide+3"
-            alt=""
-          />
+        <video className="d-block w-100" autoPlay loop muted>
+            <source src={video_pinguim} type="video/mp4" />
+            Seu navegador não suporta o elemento de vídeo.
+          </video>
           <CarouselCaption>
-
+          {/* <h3>Slide 1</h3> */}
+       
           </CarouselCaption>
+          <Link to="/SobreNosPage" className="no-link-style">
+              <ButtonCarrossel_3 variant="primary">
+                  <h2>ONGs PARCEIRAS</h2>
+              </ButtonCarrossel_3>
+          </Link> 
+          <LogoCarrossel_3 src={Logo} alt="Logo" />
         </CarouselItem>
       </Carousel>
     </CarrosselContainer>
--- src/Frontend/src/Components/SobreNos.js
+++ src/Frontend/src/Components/SobreNos.js
@@ -10,9 +10,12 @@ width: 100%;
 .titulo{
     font-size: 40px;
     font-weight: bold;
+    text-align: center;
 }
 .paragrafo{
     font-size: auto;
+    text-indent: 4em;
+    text-align: justify;
 }
 .destaque{
     font-size: 70px;
@@ -51,6 +54,7 @@ align-items: center;
 width: 100%;
 height: 100%;
 padding: 40px;
+gap: 20px;
 `
 const Frame_2 = styled.div`
 display: flex;
@@ -86,11 +90,12 @@ width: 100%;
 max-width: 1920px;
 `
 const Frame_4_1 = styled.div`
+display: flex;
+flex-direction: column;
 padding: 20px;
 width: 50%;
-p{
-    text-align: justify;
-}
+gap: 20px;
+padding: 40px;
 `
 const Frame_4_2 = styled.div`
 display: flex;
@@ -99,9 +104,6 @@ justify-content: center;
 align-items: center;
 width: 50%;
 padding: 65px;
-p{
-    text-align: center;
-}
 `
 
 
@@ -115,33 +117,33 @@ function SobreNos() {
                 </Frame_1_1>
                 <Frame_1_2>
                     <p className="titulo">Quem é a Aliança Oceânica?</p>
-                    <p className="paragrafo"> Somos alunos de Ciência da Computação do segundo semestre da Fundação Escola de Comércio Álvares Penteado (FECAP) e, como projeto acadêmico, desenvolvemos um hub digital de notícias, curiosidades e informações sobre a vida marinha. Inspirado no Objetivo de Desenvolvimento Sustentável (ODS) número 14, 'Vida na Água', o site visa sensibilizar o público sobre a preservação dos ecossistemas aquáticos e a importância da sustentabilidade marinha. Utilizando React como framework, combinamos HTML, CSS e JavaScript para criar uma plataforma interativa e responsiva que promove a educação e a conscientização ambiental. </p>
+                    <p className="paragrafo">A Aliança Oceânica é uma ONG dedicada à preservação da vida aquática e à promoção de um uso sustentável dos ecossistemas marinhos e de água doce. Nossa missão é divulgar conhecimento científico e conscientizar sobre causas e ações relacionadas à proteção desses ambientes, mobilizando doações que são direcionadas a ONGs parceiras. Além disso, buscamos fortalecer nossa influência junto a autoridades governamentais, atuando como uma ponte para viabilizar projetos de desenvolvimento sustentável que conciliem o uso socioeconômico e a conservação ecológica dos ecossistemas aquáticos em escala global. </p>
                 </Frame_1_2>
             </Frame_1>
             <Frame_2>
                 <Frame_2_1>
                     <img src="https://cdn4.iconfinder.com/data/icons/sea-life-23/64/flying_fish_fish_ocean_sea_sea_life-256.png" />
-                    <p className="destaque">+ de 15</p>
-                    <p className="titulo">espécies que visamos proteger.</p>
+                    <p className="destaque">+ de 2,2 milhões</p>
+                    <p className="titulo">de espécies pertencem ao ecossistema aquático</p>
                 </Frame_2_1>
                 <Frame_2_2>
                     <img src="https://cdn4.iconfinder.com/data/icons/geography-15/64/earth_education_globe_learning_map-256.png" />
-                    <p className="destaque">+ de 20</p>
-                    <p className="titulo">ONGs que apoiamos por todo o Brasil.</p>
+                    <p className="destaque">+ de 70%</p>
+                    <p className="titulo">de toda a área terrestre é composta pelos ecossistemas aquáticos</p>
                 </Frame_2_2>
                 <Frame_2_3>
                     <img src="https://cdn3.iconfinder.com/data/icons/space-254/64/Data-256.png" />
-                    <p className="destaque">+ de 10 </p>
-                    <p className="titulo">notícias sobre nossos oceanos.</p>
+                    <p className="destaque">+  de US$ 145 trilhões </p>
+                    <p className="titulo"> por ano que rendem as atividades exploratórias relacionadas ao ecossistema aquático no mundo</p>
                 </Frame_2_3>
             </Frame_2>
             <Frame_3>
                 <Frame_3_1>
-                    <img src="https://raw.githubusercontent.com/2024-2-MCC2/Projeto1/refs/heads/main/imagens/image-22-2.png" />
+                    <img src="https://github.com/2024-2-MCC2/Projeto1/blob/main/src/Frontend/src/Assets/ONU_CupulaODS_20230818_FotoDaONU_MarkGarten.jpg?raw=true" />
                 </Frame_3_1>
                 <Frame_3_2>
-                    <p className="titulo">O que fazemos?</p>
-                    <p className="paragrafo">Nosso site reúne de forma cuidadosa conteúdo sobre a vida marinha, trazendo notícias atualizadas de fontes confiáveis, como Greenpeace e WWF, para manter o público informado sobre iniciativas de conservação e desafios ambientais. Além disso, oferecemos uma seção de curiosidades que explora a diversidade de espécies marinhas, apresentando informações fascinantes sobre os hábitos, habitats e características únicas desses animais. Com essa combinação de atualizações relevantes e conhecimento sobre a biodiversidade marinha, nosso hub busca ser um ponto de referência para quem se interessa por temas ecológicos e pela proteção dos oceanos.</p>
+                    <p className="titulo">O que são as ODS?</p>
+                    <p className="paragrafo">Os Objetivos de Desenvolvimento Sustentável (ODS) são uma coleção de 17 objetivos globais estabelecidos pela Organização das Nações Unidas (ONU) como parte da Agenda 2030, com o propósito de enfrentar os maiores desafios globais, promovendo um desenvolvimento sustentável e equilibrado. Lançadas em 2015, as ODS buscam acabar com a pobreza, proteger o meio ambiente, promover a igualdade e assegurar prosperidade para todos, com foco em áreas como saúde, educação, igualdade de gênero, trabalho digno, redução das desigualdades, ação climática e paz. Cada objetivo contém metas específicas a serem alcançadas até 2030, e todos os países são incentivados a adaptá-los de acordo com suas próprias prioridades e contextos.</p>
                 </Frame_3_2>
             </Frame_3>
             <Frame_4>
@@ -151,7 +153,7 @@ function SobreNos() {
                 </Frame_4_1>
                 <Frame_4_2>
                     <img src="https://cdn2.iconfinder.com/data/icons/ios-7-icons/50/globe-256.png" />
-                    <p className="destaque">ODS 14</p>
+                    <p className="destaque">ODS 14:</p>
                     <p className="destaque">Vida na Água</p>
                 </Frame_4_2>
             </Frame_4>
